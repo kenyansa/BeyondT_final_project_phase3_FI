c9f7087c37a8e757b17a0e849157969b19cb20d2
Kenyansa <kenyansajustine@gmail.com>
2022-07-01 06:11:11 +0300

    introduce useState hooks to keep tract of the form data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import './App.css';
 
 const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  
   return (<div className='container'>
     <div className='app-wrapper'>
       <div>
