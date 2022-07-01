c0efafc44dbe1e341dee45a8b774ef545f347381
Kenyansa <kenyansajustine@gmail.com>
2022-07-01 06:01:12 +0300

    create form component

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+        <input type="text" placeholder="Enter your next task..." className='task-input' />
+    </form>
+  )
+}
+
+export default Form
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>BeyondT Planner</h1>
+    </div>
+  )
+}
+
+export default Header
