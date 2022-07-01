4d1a0aff037e8b45681adfdd064fc6044c6d7bbe
Kenyansa <kenyansajustine@gmail.com>
2022-07-01 08:37:55 +0300

    remove fa buttons and replace with the manual ones

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,23 +8,17 @@ const TodoList = ({todos, setTodos}) => {
                 <input type="text" 
                 value={todo.title} 
                 className="list" 
-                onChange={(event)=>event.preventDefault()} 
-                />
+                onChange={(event)=> 
+                event.preventDefault()} />
                 <div>
-                    <button className='button-complete task-button'>
-                        <i className='fa fa-check-circle'></i>
-                    </button>
-                    <button className='button-edit task-button'>
-                        <i className='fa fa-edit'></i>
-                    </button>
-                    <button className='button-delete task-button'>
-                        <i className='fa fa-trash'></i>
-                    </button>
+                    <button className='button-complete'>Done</button>
+                    <button className='button-edit'>Edit</button>
+                    <button className='button-delete'>Del</button>
                 </div>
             </li>
-        )
-        )}
-    </div>);
+        ))}
+    </div>
+    );
 };
 
 export default TodoList
