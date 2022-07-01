c24fdb6f997fa0e1db650ef294e4a5c176692ab7
Kenyansa <kenyansajustine@gmail.com>
2022-07-01 06:04:39 +0300

    import form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 import './App.css';
 
 const App = () => {
@@ -7,6 +8,9 @@ const App = () => {
     <div className='app-wrapper'>
       <div>
          <Header />
+      </div>
+      <div>
+        <Form />
       </div>
     </div>
   </div>
